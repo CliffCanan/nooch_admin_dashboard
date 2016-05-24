d08c7a45a2f9d16cb24a9ab0a755cd7e47f15f29
malkitsingh <malkitsingh01@gmail.com>
2016-05-24 16:17:09 +0530

    implemented hangfire into nooch admin project

---
+++ New Admin Panel/noochAdminNew/noochAdminNew/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.Owin;
+using noochAdminNew.Classes.Utility;
+using Owin;
+
+[assembly: OwinStartup(typeof(noochAdminNew.Startup))]
+
+namespace noochAdminNew
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+
+            // setting up hangfire
+            GlobalConfiguration.Configuration.UseSqlServerStorage("data source=54.201.43.89;initial catalog=NOOCH;user id=sa;password=Singh@123;");
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+
+            //RecurringJob.AddOrUpdate(() => Logger.Info("Auto Task Running"), "0 12 * */2");
+            RecurringJob.AddOrUpdate(() => Logger.Info("Auto Task Running"), Cron.Minutely);
+        }
+    }
+}
---
+++ New Admin Panel/noochAdminNew/noochAdminNew/Startup2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.Owin;
+using Owin;
+using noochAdminNew.Classes.Utility;
+
+[assembly: OwinStartup(typeof(noochAdminNew.Startup2))]
+
+namespace noochAdminNew
+{
+    public class Startup2
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            Logger.Info("Owin startup hit 2");
+
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            GlobalConfiguration.Configuration.UseSqlServerStorage("NOOCHEntities");
+            // configuring hangfire
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
