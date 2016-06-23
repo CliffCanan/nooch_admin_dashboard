f133290fa4afb733a3c7a13f7b85a13fd7220dfa
malkitsingh <malkitsingh01@gmail.com>
2016-06-23 15:32:49 +0530

    tried auto mapper lib for server side sorting, pagination and searching on all members, landlords and tenants table

--- New Admin Panel/noochAdminNew/noochAdminNew/Controllers/ServicesController.cs
+++ New Admin Panel/noochAdminNew/noochAdminNew/Controllers/ServicesController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Helpers;
 using System.Web.Http;
+using AutoMapper;
 using noochAdminNew.Classes.Utility;
 using noochAdminNew.Models;
 
@@ -26,7 +27,7 @@ namespace noochAdminNew.Controllers
 
                 // getting all users with active knox bank account
 
-                var allActiveUsersWithKnox = (from c in obj.Members where c.IsVerifiedWithSynapse== true && c.IsDeleted==false select c).ToList();
+                var allActiveUsersWithKnox = (from c in obj.Members where c.IsVerifiedWithSynapse == true && c.IsDeleted == false select c).ToList();
 
                 foreach (Member kad in allActiveUsersWithKnox)
                 {
@@ -53,6 +54,45 @@ namespace noochAdminNew.Controllers
         public class MemberSearchResult
         {
             public string UserName { get; set; }
+        }
+
+        [HttpGet]
+        [ActionName("TestingPaginationForAllMembersPage")]
+        public void TestingPaginationForAllMembersPage()
+        {
+            using (NOOCHEntities obj = new NOOCHEntities())
+            {
+
+                var AllMembers = obj.Members.Take(20).Where(m => m.IsDeleted == false).ToList();
+
+
+
+                Mapper.Initialize(cfg => cfg.CreateMap<Member, MemberPOCO>()
+
+                    // could be blank things
+                    .ForMember(dest => dest.UserName, dest => dest.MapFrom((src => src.UserName != null ? CommonHelper.GetDecryptedData(src.UserName) : "")))
+                    .ForMember(dest => dest.UserNameLowerCase, dest => dest.MapFrom((src => src.UserNameLowerCase != null ? CommonHelper.GetDecryptedData(src.UserNameLowerCase) : "")))
+                    .ForMember(dest => dest.FirstName, dest => dest.MapFrom((src => src.FirstName != null ? CommonHelper.GetDecryptedData(src.FirstName) : "")))
+                    .ForMember(dest => dest.LastName, dest => dest.MapFrom((src => src.LastName != null ? CommonHelper.GetDecryptedData(src.LastName) : "")))
+
+                    //could be null...address related stuff
+                    .ForMember(dest => dest.City, dest => dest.MapFrom((src => src.City != null ? CommonHelper.GetDecryptedData(src.City) : "")))
+                    .ForMember(dest => dest.Address, dest => dest.MapFrom((src => src.Address != null ? CommonHelper.GetDecryptedData(src.Address) : "")))
+                    .ForMember(dest => dest.State, dest => dest.MapFrom((src => src.State != null ? CommonHelper.GetDecryptedData(src.State) : "")))
+                    .ForMember(dest => dest.Zipcode, dest => dest.MapFrom((src => src.Zipcode != null ? CommonHelper.GetDecryptedData(src.Zipcode) : "")))
+
+                    .ForMember(dest => dest.MemberId, dest => dest.MapFrom((src => new Guid(src.MemberId.ToString()))))
+                    
+                    .ForMember(dest => dest.ContactNumber, dest => dest.MapFrom((src => src.ContactNumber)))
+                    .ForMember(dest => dest.DateCreated, dest => dest.MapFrom((src => src.DateCreated)))
+                    .ForMember(dest => dest.Status, dest => dest.MapFrom((src => src.Status)))
+                    
+
+                    );
+
+                var list = Mapper.Map<List<Member>, List<MemberPOCO>>(AllMembers);
+
+            }
         }
     }
 }
